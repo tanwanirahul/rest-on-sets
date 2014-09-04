3f5ee92d3f614faec38a9672f3c5c17ffd188828
tanwairahul <tanwanirahul@gmail.com>
2014-09-04 15:30:23 +0530

    Adding a custom DoesNotExist exception

--- SetOperations/core/exceptions.py
+++ SetOperations/core/exceptions.py
@@ -5,3 +5,15 @@ Created on 01-Sep-2014
 
 Description: Home for all the custom implemented exceptions.
 '''
+
+
+class DoesNotExist(Exception):
+    '''
+        Raised when we try to access a resource that does not exist
+    '''
+
+    def __init__(self, *args, **kwargs):
+        '''
+            Initialize.
+        '''
+        Exception.__init__(self, *args, **kwargs)
