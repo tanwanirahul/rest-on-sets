4d48f08b09b00b3360532e8936c7182270f6b8bd
tanwairahul <tanwanirahul@gmail.com>
2014-09-04 15:29:54 +0530

    Adding implementation for place holder operaions

--- SetOperations/core/datastore.py
+++ SetOperations/core/datastore.py
@@ -6,6 +6,8 @@ Created on 01-Sep-2014
 Description: Holds the in memory data store implementation and infrastructure.
 '''
 from utils import get_unique_identifier
+from SetOperations.core.utils import ObjLikeDict
+from SetOperations.core.exceptions import DoesNotExist
 
 
 class InMemDataStore(object):
@@ -23,21 +25,56 @@ class InMemDataStore(object):
         '''
         _id, doc = cls._pre_process(doc)
         cls._data_store.update(doc)
-        return _id
+        return ObjLikeDict(doc[_id])
 
     @classmethod
     def find(cls, doc_id):
         '''
             Finds and returns the document based on the id.
         '''
-        return cls._data_store.get(doc_id)
+        req_obj = cls._data_store.get(doc_id, None)
+        if req_obj is None:
+            raise DoesNotExist()
+        return ObjLikeDict(req_obj)
 
     @classmethod
     def find_all(cls):
         '''
             Returns all the set documents in the system.
         '''
-        return cls._data_store.values()
+        return [ObjLikeDict(doc) for doc in cls._data_store.values()]
+
+    @classmethod
+    def remove(cls, doc_id):
+        '''
+            Removes the document with doc_id from the datastore.
+        '''
+        obj_deleted = cls._data_store.pop(doc_id, None)
+        if obj_deleted is None:
+            raise DoesNotExist()
+        return ObjLikeDict(obj_deleted)
+
+    # TODO: Make these operations generic with no assumptions for this app.
+    @classmethod
+    def add_members(cls, doc_id, members):
+        '''
+            Add members in the given document. This is getting specific to set
+            operations..
+        '''
+        obj = cls.find(doc_id)
+        obj.members.add_all(members)
+        return obj
+
+    # TODO: Make these operations generic with no assumptions for this app.
+    @classmethod
+    def remove_members(cls, doc_id, members):
+        '''
+            Add members in the given document. This is getting specific to set
+            operations..
+        '''
+        obj = cls.find(doc_id)
+        obj.members.remove_all(members)
+        return obj
 
     @classmethod
     def _pre_process(cls, doc):
