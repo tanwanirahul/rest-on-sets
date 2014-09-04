92ef671675db1f6227b7827d0c6b62308fca386e
tanwairahul <tanwanirahul@gmail.com>
2014-09-04 15:31:44 +0530

    Using INMemory Data store to hold the set objects

--- SetOperations/urls.py
+++ SetOperations/urls.py
@@ -3,8 +3,10 @@ from django.conf.urls import patterns, include
 from tastypie.api import Api
 
 from SetOperations.core.resources import SetOperationsResource
+from SetOperations.core.datastore import InMemDataStore
+
 api_v1 = Api(api_name=settings.API_VERSION)
-api_v1.register(SetOperationsResource(settings.API_VERSION, None))
+api_v1.register(SetOperationsResource(InMemDataStore, settings.API_VERSION))
 
 urlpatterns = patterns('',
                        (r'^api/', include(api_v1.urls)),
