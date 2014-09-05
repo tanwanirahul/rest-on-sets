da3df25b581b87aef9ccdc9a4b26f43a9f9ff45d
tanwairahul <tanwanirahul@gmail.com>
2014-09-05 11:51:25 +0530

    Adding a decorator for type check

--- SetOperations/core/utils.py
+++ SetOperations/core/utils.py
@@ -6,8 +6,9 @@ Created on 01-Sep-2014
 Description: Contains all the reusable utility functions (pure functions).
 '''
 from uuid import uuid4
-from SetOperations.core.exceptions import DoesNotExist
+from SetOperations.core.exceptions import DoesNotExist, NotAnHSet
 from tastypie.exceptions import NotFound
+from SetOperations.core.hset import HSet
 
 
 def get_unique_identifier():
@@ -42,4 +43,19 @@ def handle_does_not_exist(func):
             return func(*args, **kwars)
         except DoesNotExist:
             raise NotFound("The requested resource does not exist")
+    return wrapper
+
+
+def type_check(func):
+    '''
+        A decorator to wrap and perform type check of HSet.
+    '''
+    def wrapper(*args, **kwars):
+        '''
+            Wrapps the function call - func()
+        '''
+        failed_types = [arg for arg in args if type(arg) not in (HSet,)]
+        if failed_types:
+            raise NotAnHSet("The arguments passed are not of a HSet type.")
+        return func(*args, **kwars)
     return wrapper
